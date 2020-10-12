6eb42bbb5c0f0bb5de50e3d4874b3c2790e82fc6
vasu1124 <vasu1124@actvirtual.com>
2020-10-12 20:06:57 +0200

    configure reconciliation interval

--- src/main.ts
+++ src/main.ts
@@ -11,21 +11,29 @@ const cli = program
   .name("vasu1124/ngrok")
   .version(version, '-v, --version', 'output the current version')
   .usage("[Options]")
-  .option("-l, --log <level>", "log level [ALL < TRACE < DEBUG < INFO < WARN < ERROR < FATAL < MARK < OFF]", "debug")
+  .option("-l, --log <level>", "log level [ALL < TRACE < *DEBUG* < INFO < WARN < ERROR < FATAL < MARK < OFF]", "debug")
+  .option("-i, --interval <ms>", "reconcile interval [60000]ms", "60000")
   .parse(process.argv);
 
 
 // ------------------------------------------------
-// Set global log level options
+// Set global options
 // ------------------------------------------------
 
 const logger = getLogger("ngrok");
 logger.level = cli.log;
+let interval = Number(cli.interval);
+if (Number.isNaN(interval))
+{
+  interval = 60*1000;
+  cli.interval = `${interval}`;
+}
 
 logger.debug(cli.opts());
 
+
 const main = async (options: any) => {
-  const operator = new NgrokContoller(logger);
+  const operator = new NgrokContoller(logger, interval);
   await operator.start();
 
   const exit = (reason: string) => {
